b13c24b9494cf845f6e1cfbef283431dc731f9d7
Stefan Fischer <stefischer@mail.com>
2023-05-03 13:23:15 -0400

    Create utils directory with navigation utils

---
+++ client/src/utils/navigation.ts
@@ -0,0 +1,8 @@
+import { Route } from '../types/routes';
+
+export function isActiveRoute(currentPathname: string, route: Route): boolean {
+    const re = /^.+?(?=\/|$)/g;
+    const matchRes = currentPathname.match(re)![0];
+
+    return matchRes === route.path;
+};
