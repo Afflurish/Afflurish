2a4b12583b332100809ca37c97fe726560456b9c
Stefan Fischer <stefischer@mail.com>
2023-05-11 11:01:06 -0400

    Update User Entity

--- api/src/entities/User.ts
+++ api/src/entities/User.ts
@@ -3,28 +3,46 @@ import {
     BaseEntity, 
     PrimaryGeneratedColumn, 
     Column, 
-    CreateDateColumn
+    CreateDateColumn,
+    UpdateDateColumn,
+    Unique
 } from 'typeorm';
 
 @Entity('users')
+@Unique(['email'])
 class User extends BaseEntity {
 
     constructor(
-        id: number,
+        id: string,
         email: string,
+        display_name: string,
+        password: string,
+        updated_at: Date,
         created_at: Date
     ) {
         super();
         this.id = id;
         this.email = email;
+        this.display_name = display_name;
+        this.password = password;
+        this.updated_at = updated_at;
         this.created_at = created_at;
     };
 
     @PrimaryGeneratedColumn("uuid")
-    id: number;
+    id: string;
 
     @Column({ type: "varchar", length: 255 })
     email: string;
+
+    @Column({ type: "varchar", length: 255, nullable: true })
+    display_name: string;
+
+    @Column({ type: "varchar", length: 255 })
+    password: string;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 
     @CreateDateColumn()
     created_at: Date;
