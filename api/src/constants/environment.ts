218e161731004f445df8b15f73b814b7fc8bf611
Stefan Fischer <stefischer@mail.com>
2023-05-11 11:01:22 -0400

    Add JWT_TOKEN to environment constants

--- api/src/constants/environment.ts
+++ api/src/constants/environment.ts
@@ -8,3 +8,5 @@ export const IS_TEST = process.env.NODE_ENV === "test";
 export const BASE_URL = process.env.BASE_URL as string;
 
 export const API_PORT = process.env.API_PORT as string;
+
+export const JWT_TOKEN = process.env.JWT_TOKEN as string;
