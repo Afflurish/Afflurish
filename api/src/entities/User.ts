97204d8c72acf0dd7efdfb9078afe70b0db336f9
Stefan Fischer <stefischer@mail.com>
2023-05-24 21:50:30 -0400

    Update User entity to use join columns

--- api/src/entities/User.ts
+++ api/src/entities/User.ts
@@ -5,8 +5,13 @@ import {
     Column, 
     CreateDateColumn,
     UpdateDateColumn,
+    OneToMany,
     Unique
 } from 'typeorm';
+
+import IncomeSource from './IncomeSource.js';
+import BudgetCategory from './BudgetCategory.js';
+import Expense from './Expense.js';
 
 @Entity('users')
 @Unique(['email'])
@@ -18,7 +23,11 @@ class User extends BaseEntity {
         display_name: string,
         password: string,
         updated_at: Date,
-        created_at: Date
+        created_at: Date,
+
+        income_sources: IncomeSource[],
+        budget_categories: BudgetCategory[],
+        expenses: Expense[]
     ) {
         super();
         this.id = id;
@@ -27,6 +36,10 @@ class User extends BaseEntity {
         this.password = password;
         this.updated_at = updated_at;
         this.created_at = created_at;
+
+        this.income_sources = income_sources;
+        this.budget_categories = budget_categories;
+        this.expenses = expenses;
     };
 
     @PrimaryGeneratedColumn("uuid")
@@ -46,6 +59,28 @@ class User extends BaseEntity {
 
     @CreateDateColumn()
     created_at: Date;
+
+    /* Relations */
+    @OneToMany(
+        () => IncomeSource,
+        (income_sources: IncomeSource) => income_sources.user,
+        { onDelete: "CASCADE" }
+    )
+    income_sources: IncomeSource[];
+
+    @OneToMany(
+        () => BudgetCategory,
+        (budget_categories: BudgetCategory) => budget_categories.user,
+        { onDelete: "CASCADE" }
+    )
+    budget_categories: BudgetCategory[];
+
+    @OneToMany(
+        () => Expense,
+        (expenses: Expense) => expenses.user,
+        { onDelete: "CASCADE" }
+    )
+    expenses: Expense[]
 };
 
 export default User;
