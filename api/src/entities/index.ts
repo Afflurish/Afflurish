c8aa562817c297aa1dd02ae536eb9d1a39c2d2e7
Stefan Fischer <stefischer@mail.com>
2023-05-24 21:50:42 -0400

    Add new entities to entity index

--- api/src/entities/index.ts
+++ api/src/entities/index.ts
@@ -1,1 +1,4 @@
+export { default as BudgetCategory } from './BudgetCategory.js';
+export { default as Expense } from './Expense.js';
+export { default as IncomeSource } from './IncomeSource.js';
 export { default as User } from './User.js';
