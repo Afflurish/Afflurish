323e176fcc2b8ed29ed5d3b11075711e8cd83d4a
Stefan Fischer <stefischer@mail.com>
2023-05-24 21:54:03 -0400

    Create date types and utils

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,4 @@
+export * as apiRequests from './apiRequests';
+export * as dates from './dates';
 export * as navigation from './navigation';
 export * as promises from './promises';
-export * as apiRequests from './apiRequests';
