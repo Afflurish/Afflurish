a50baa3973144c3c792e2793e8c5e28e0d6d3c86
Stefan Fischer <stefischer@mail.com>
2023-06-01 00:26:50 -0400

    Create placeholder landing page content

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,13 +1,73 @@
-import { Flowbite } from 'flowbite-react';
+import Head from 'next/head';
+import Lottie from 'lottie-react';
+import { Flex, Box } from 'reflexbox';
+import { motion } from 'framer-motion';
 
-import Navigation from '../navigation';
+/* Redux */
+import { store } from '../store';
 
-import * as themes from '../theme';
+import dashboardAnimation from '../lottie/dashboard-analytics.json';
+import { Button } from '../components/Common';
 
 function Home() {
+
+    const theme = store.getState().theme;
+
     return (
-        <div>
-        </div>
+        <Flex>
+            <Head>
+                <title>Afflurish</title>
+            </Head>
+            <Flex className="w-full" justifyContent="center">
+                <Box className="flex flex-1 flex-wrap justify-center content-center items-center">
+                    <motion.div 
+                        initial={{ x: "-50vw" }}
+                        animate={{ x: 1 }}
+                        transition={{ duration: 4, type: "spring", mass: .5 }}
+                        className="flex flex-col justify-center w-6/12"
+                    >
+                        <p className="text-xl mb-1.5">Track. Budget. Analyze.</p>
+                        <h1 
+                            className={`
+                                ${theme.tailwind.gradientText}
+                                text-6xl
+                                font-bold
+                                text-left
+                                mb-5
+                            `}
+                        >
+                            Tools For Better<br />Personal Finance
+                        </h1>
+                        <p className="mb-7">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            sed do eiusmod tempor incididunt ut labore et dolore magna 
+                            aliqua. Etiam sit amet nisl purus. Lectus magna fringilla 
+                            urna porttitor rhoncus dolor purus non.
+                        </p>
+                        <Flex className="gap-4">
+                            <Button color="gradient" size="lg">
+                                Learn More
+                            </Button>
+                            <Button outlined color="gradient" size="lg">
+                                Learn More
+                            </Button>
+                        </Flex>
+                    </motion.div>
+                </Box>
+                <Box className="flex flex-1 items-center justify-center">
+                    <motion.div
+                        initial={{ x: "50vw" }}
+                        animate={{ x: 1 }}
+                        transition={{ duration: 4, type: "spring", mass: .5, delay: .5 }}
+                    >
+                    <Lottie 
+                        className="w-8/12 h-8/12 mt-14"
+                        animationData={dashboardAnimation} 
+                    />
+                    </motion.div>
+                </Box>
+            </Flex>
+        </Flex>
     );
 };
 
