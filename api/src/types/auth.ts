3894fca170b81f5874dee5fddd19ce8a2a38230f
Stefan Fischer <stefischer@mail.com>
2023-06-13 17:16:24 -0400

    Add new Auth types

--- api/src/types/auth.ts
+++ api/src/types/auth.ts
@@ -1,6 +1,10 @@
-export interface AuthPayload {
+export interface AuthUser {
     id: string,
     email: string,
     display_name: string,
+};
+
+export interface AuthPayload {
+    user: AuthUser,
     token?: string
 };
